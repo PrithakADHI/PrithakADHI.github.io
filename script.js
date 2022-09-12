56cca2734f4060fe76301ab531753ffcb02de45c
PrithakADHI <80581730+PrithakADHI@users.noreply.github.com>
2022-09-12 14:53:15 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function w3_open() {
+    if(document.getElementById("mySidebar").style.display == "none") {
+      document.getElementById("mySidebar").style.display = "block";
+    } else {
+      document.getElementById("mySidebar").style.display = "none"
+    }
+  }
